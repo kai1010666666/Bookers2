ff60a3244df3f136955b7b7219a48d7922654fc5
EC2 Default User <ec2-user@ip-172-31-39-139.ap-northeast-1.compute.internal>
2023-04-24 12:16:15 +0000

    Bookers2 top=>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -44,6 +44,6 @@ class BooksController < ApplicationController
   private
   
   def book_params
-    params.require(:book).permit(:title,:image, :body)
+    params.require(:book).permit(:title,:image,:body)
   end
 end
